aab1e0ea67210bfe4eb0b9f556678a205e46a2de
Manioker <andithevac@gmail.com>
2025-04-25 13:16:33 +0200

    test map

--- src/utils/valid_map.c
+++ src/utils/valid_map.c
@@ -6,7 +6,7 @@
 /*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:13:15 by rothiery          #+#    #+#             */
-/*   Updated: 2025/04/25 12:28:55 by anvacca          ###   ########.fr       */
+/*   Updated: 2025/04/25 13:08:54 by anvacca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ bool	valid_map(char **map, unsigned int y, unsigned int x)
 		&& map[y][x + 1] != 'N' && map[y][x + 1] != 'S'
 		&& map[y][x + 1] != 'E' && map[y][x + 1] != 'W'))
 		return (false);
-	else if (y == arr_len(map) || (map[y + 1][x] != '1'
+	else if (y == arr_len(map) - 1 || (map[y + 1][x] != '1'
 		&& map[y + 1][x] != '0'
 		&& map[y + 1][x] != 'N' && map[y + 1][x] != 'S'
 		&& map[y + 1][x] != 'E' && map[y + 1][x] != 'W'))
