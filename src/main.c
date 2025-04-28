51e89b42ffefa56936094b2dd5a3a81a34e1ff22
Romtry <romaindogcn@gmail.com>
2025-04-28 12:41:29 +0200

    gitattribute

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 13:21:39 by anvacca           #+#    #+#             */
-/*   Updated: 2025/04/25 12:57:44 by rothiery         ###   ########.fr       */
+/*   Updated: 2025/04/25 13:18:33 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 	if (parser(argc, argv, &game))
 		game_loop(&mlx, &game);
 	else
-		printf("Wrong map");
+		printf("Wrong map\n");
 	free_game(&game);
 	return (0);
 }
--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 10:51:26 by rothiery          #+#    #+#             */
-/*   Updated: 2025/04/25 12:52:42 by rothiery         ###   ########.fr       */
+/*   Updated: 2025/04/25 14:20:51 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ static bool	skip_nl(int fd, char **line)
 	*line = get_next_line(fd);
 	while (*line)
 	{
+		printf("[%s]\n", *line);
 		if (**line != '\n' && **line != '\0')
 			return (true);
 		free(*line);
 		*line = get_next_line(fd);
 	}
-	return (printf("no map\n"), false);
+	return (printf("no map\n"), free(*line), false);
 }
 
 static bool	is_map(char *line)
@@ -38,7 +39,7 @@ static bool	is_map(char *line)
 		if (line[i] != ' ' && line[i] != '0' && line[i] != '1'
 			&& line[i] != 'N' && line[i] != 'S' && line[i] != 'E'
 			&& line[i] != 'W')
-			return (printf("invalid char\n"), false);
+			return (printf("invalid char\n"), free(line), false);
 		i++;
 	}
 	return (true);
@@ -94,10 +95,8 @@ bool	parse_map(int fd, t_game *game)
 {
 	char			*line;
 	unsigned int	i;
-	char			**map;
 
 	i = 0;
-	map = NULL;
 	if (!skip_nl(fd, &line))
 		return (false);
 	while (line)
@@ -105,12 +104,11 @@ bool	parse_map(int fd, t_game *game)
 		if (!is_map(line))
 			return (false);
 		else
-			map = get_array(i, map, line);
+			game->map = get_array(i, game->map, line);
 		i++;
 		line = get_next_line(fd);
 	}
-	if (!check_map(map, game))
-		return (free(map), false);
-	game->map = map;
+	if (!check_map(game->map, game))
+		return (false);
 	return (true);
 }
