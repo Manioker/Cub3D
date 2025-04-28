c56b334e72c0fd3c26834faf3acd382fc9fe5778
Romtry <romaindogcn@gmail.com>
2025-04-28 12:45:06 +0200

    end_game

--- src/game/end_game.c
+++ src/game/end_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   end_game.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:38:28 by anvacca           #+#    #+#             */
-/*   Updated: 2025/04/25 13:07:43 by anvacca          ###   ########.fr       */
+/*   Updated: 2025/04/28 12:44:41 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	end_game(t_main_s *main_s)
 {
-	(void)main_s;
+	if (main_s->mlx->mlx_win)
+		mlx_destroy_window(main_s->mlx->mlx, main_s->mlx->mlx_win);
+	if (main_s->mlx->data.img)
+		mlx_destroy_image(main_s->mlx->mlx, main_s->mlx->data.img);
+	//free
 	exit(0);
 }
