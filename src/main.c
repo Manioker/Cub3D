7d4286639517f48363afbec894e1a1bd36a619cb
Manioker <andithevac@gmail.com>
2025-05-09 13:35:33 +0200

    small fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 13:21:39 by anvacca           #+#    #+#             */
-/*   Updated: 2025/05/09 12:32:52 by anvacca          ###   ########.fr       */
+/*   Updated: 2025/05/09 13:35:07 by anvacca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ int	main(int argc, char **argv)
 	t_game	game;
 
 	game.map = NULL;
+	game.so_path = NULL;
+	game.no_path = NULL;
+	game.we_path = NULL;
+	game.ea_path = NULL;
 	if (parser(argc, argv, &game))
 		game_loop(&mlx, &game);
 	else
